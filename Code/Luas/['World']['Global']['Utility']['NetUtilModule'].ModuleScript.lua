99311543865fe301176c663470ecba44f2362d01
YenYuan <64084912+lilith-avatar-yenyuan@users.noreply.github.com>
2020-05-18 16:26:33 +0800

    客户端广播事件添加显示信息方法

--- Code/Luas/['World']['Global']['Utility']['NetUtilModule'].ModuleScript.lua
+++ Code/Luas/['World']['Global']['Utility']['NetUtilModule'].ModuleScript.lua
@@ -29,15 +29,13 @@ function NetUtil.static.Fire_S(_eventName, ...)
         return
     end
     world.S_Event[_eventName]:Fire(...)
-    --print(...)
 end
 
 --- 客户端广播
 -- @param @string _eventName 事件的名字(严格对应)
 -- @param ... 事件参数
-function NetUtil.static.Broadcast(_eventName, ...)
-    print(string.format('[信息] 客户端广播事件: %s', _eventName))
-    world.Players:BroadcastEvent(_eventName, ...)
+function NetUtil.Broadcast(_eventName, ...)
+	world.Players:BroadcastEvent(_eventName, ...)
 end
 
 --- 向指定的玩家发送消息,并显示事件信息
@@ -46,9 +44,8 @@ end
 -- @param ... 事件参数
 function NetUtil.static.Fire_C_Show(_eventName, _player,  ...)
     NetUtil.Fire_C(_eventName,_player, ...)
-    print(string.format('[信息] 客户端事件: %s , 玩家: %s', _eventName, _player.Name))
+    print(string.format('[信息] 客户端事件: %s , 玩家: ', _eventName, _player.Name))
 end
-
 
 ---向服务端发送消息,并显示事件信息
 ---@param _eventName string
@@ -58,5 +55,12 @@ function NetUtil.static.Fire_S_Show(_eventName, ...)
     print(string.format('[信息] 服务器事件: %s', _eventName))
 end
 
+--- 客户端广播，并显示事件信息
+-- @param @string _eventName 事件的名字(严格对应)
+-- @param ... 事件参数
+function NetUtil.Broadcast_Show(_eventName, ...)
+    NetUtil.Broadcast(_eventName, ...)
+    print(string.format('[信息] 客户端广播事件: %s ,参数为：%s ', _eventName, ...))
+end
 
 return NetUtil
