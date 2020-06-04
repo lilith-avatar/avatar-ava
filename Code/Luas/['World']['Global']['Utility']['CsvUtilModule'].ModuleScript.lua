7cca18e3393f98184e3b14c10f1eef1e44196690
Yuancheng Zhang <zhangyuancheng@lilith.com>
2020-06-04 23:10:52 +0800

    更新CSV读表方法，支持N个主键

--- Code/Luas/['World']['Global']['Utility']['CsvUtilModule'].ModuleScript.lua
+++ Code/Luas/['World']['Global']['Utility']['CsvUtilModule'].ModuleScript.lua
@@ -7,9 +7,8 @@ local CsvUtil = {}
 
 --- 读取配置表,会根据id生成lua表
 -- @param _csv 表格
--- @parma _id 表格的
--- @parma _isPrimaryKey 是否为主键,默认值为true
--- @usage exmaple #1 如果 _isPrimaryKey == true, 单一键值为主键
+-- @parma ... 表格的主键ids,可以为单一主键或多主键(多主键的id顺序决定lua table的结构)
+-- @usage exmaple #1 如果, 单一键值为主键
 -- Level.csv 表格内容为:
 -- ----------------------------------
 -- | String   | String     | Int    |
@@ -19,7 +18,7 @@ local CsvUtil = {}
 -- | hard_01  | Level 03   | 280    |
 -- | hard_02  | Level 04   | 320    |
 -- ----------------------------------
--- 调用函数 local levelCsv = CsvUtil.GetCsvInfo(Level, 'level_id', true) 导入的lua表格结果为:
+-- 调用函数 local levelCsv = CsvUtil.GetCsvInfo(Level, 'level_id') 导入的lua表格结果为:
 -- levelCsv = {
 --     easy_01 = {
 --         level_id = 'easy_01',
@@ -42,7 +41,7 @@ local CsvUtil = {}
 --         reward = 320
 --     }
 -- }
--- @usage exmaple #2 如果 _isPrimaryKey == false, 多键值为主键
+-- @usage exmaple #2 如果, 多键值为主键
 -- Enemy.csv 表格内容为:
 -- ----------------------------------
 -- | String   | String     | Int    |
@@ -52,76 +51,43 @@ local CsvUtil = {}
 -- | foe_02   | easy       | 300    |
 -- | foe_02   | hard       | 400    |
 -- ----------------------------------
--- 调用函数 local enemyCsv = CsvUtil.GetCsvInfo(Enemy, 'enemy_id', false) 导入的lua表格结果为:
+-- 调用函数 local enemyCsv = CsvUtil.GetCsvInfo(Enemy, 'enemy_id', 'difficulty') 导入的lua表格结果为:
 -- enemyCsv = {
 --     foe_01 = {
---         [1] = {
+--         easy = {
 --             enemy_id = 'foe_01',
 --             difficulty = 'easy',
 --             hp = 100
 --         },
---         [2] = {
+--         hard = {
 --             enemy_id = 'foe_02',
 --             difficulty = 'hard',
 --             hp = 150
 --         }
 --     },
 --     foe_02 = {
---         [1] = {
+--         esay = {
 --             enemy_id = 'foe_02',
 --             difficulty = 'easy',
 --             hp = 300
 --         },
---         [2] = {
+--         hard = {
 --             enemy_id = 'foe_02',
 --             difficulty = 'hard',
 --             hp = 400
 --         }
 --     }
 -- }
-function CsvUtil.GetCsvInfo(_csv, _id, _isPrimaryKey)
-    _isPrimaryKey = _isPrimaryKey or _isPrimaryKey == nil -- default is true
-    local tmp = _csv:GetRows()
-
-    if _id == 'Type' then
-        info(string.format('CSV数据载入, CSV:%s, key:%s', _csv.Name, _id))
-        return tmp
-    end
-
-    local result = {}
-    for k, v in pairs(tmp) do
-        if v[_id] == nil then
-            error(string.format('CSV缺少参数, CSV:%s, _id:%s', _csv.Name, _id))
-            return
-        end
-        if _isPrimaryKey then
-            -- id是唯一主键
-            if result[v[_id]] ~= nil then
-                warn(string.format('CSV数据覆盖, CSV:%s, _id:%s', _csv.Name, _id))
-            end
-            result[v[_id]] = v
-            v.Type = tonumber(k)
-        else
-            -- id不是主键,合并同id的数据
-            if result[v[_id]] == nil then
-                result[v[_id]] = {}
-            end
-            table.insert(result[v[_id]], v)
-        end
-    end
-
-    info(string.format('CSV数据载入, CSV:%s, key:%s', _csv.Name, _id))
-    return result
-end
-
-function CsvUtil.GetCsvNew(_csv, ...)
+-- 使用lua table中的数据方法:
+-- health = enemyCsv.foe_01.hard.hp 或 health = enemyCsv['foe_01']['hard']['hp']
+-- health的值为150
+function CsvUtil.GetCsvInfo(_csv, ...)
     local rawTable = _csv:GetRows()
     local ids = {...}
     if #ids < 1 or (#ids == 1 and ids[1] == 'Type') then
         -- 直接返回
         return rawTable
     end
-    table.dump(ids)
     local result = {}
     local tmp = result
     local key, id
@@ -130,8 +96,7 @@ function CsvUtil.GetCsvNew(_csv, ...)
         for i = 1, #ids do
             id = ids[i]
             key = v[id]
-            if key == nil then
-                table.dump(v)
+            if string.isnilorempty(key) then
                 error(string.format('CSV表格没有找到此id, CSV:%s, id: %s', _csv.Name, id))
             end
             if i == #ids then
@@ -151,7 +116,6 @@ function CsvUtil.GetCsvNew(_csv, ...)
             end
         end
     end
-    print('==========================================')
     return result
 end
 
