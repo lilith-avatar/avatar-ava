e760845547563e4e7bb5f6f4b082a4cdf0b994a7
Xinwu Zhang <64084648+lilith-avatar-sidzhang@users.noreply.github.com>
2020-06-22 13:50:02 +0800

    引导UI新增一个是否强制引导参数,若填写false则不会显示黑色背景蒙版

--- Code/Luas/['World']['Global']['Plugin']['FUNC_Guide']['GuideSystemModule'].ModuleScript.lua
+++ Code/Luas/['World']['Global']['Plugin']['FUNC_Guide']['GuideSystemModule'].ModuleScript.lua
@@ -14,11 +14,15 @@ GuideSystem.Enum = {
 ---@param _type Int 1:点击
 ---@param _position Vector2 生成引导UI在屏幕的位置,Anchors值
 ---@param _area Vector2 响应范围,Size
----@param _content String 文本介绍,nil则不显示文本
-function GuideSystem:ShowGuide(_type, _position, _area, _content, _callBack, ...)
+---@param _content string 文本介绍,nil则不显示文本
+---@param _isForce boolean 是否强制引导
+function GuideSystem:ShowGuide(_type, _position, _area, _content, _isForce, _callBack, ...)
 	local args = {...}
 	if _type == GuideSystem.Enum.ClickGuide then
 		local GuideNode = world:CreateInstance('ClickGuide', 'ClickGuide', localPlayer.Local)
+		if _isForce == false then
+			GuideNode.FigBackground.Visible = false
+		end
 		if _position then
 			GuideNode.ImgDot.AnchorsX = Vector2(_position.X, _position.X)
 			GuideNode.ImgDot.AnchorsY = Vector2(_position.Y, _position.Y)
