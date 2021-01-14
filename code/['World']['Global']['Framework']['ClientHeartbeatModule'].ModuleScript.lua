88985ed2fbfe2bb8da0ea4aaf917b07d8eeccb2d
Yuancheng Zhang <old.far.hot@gmail.com>
2021-01-14 21:28:06 +0800

    Update ['World']['Global']['Framework']['ClientHeartbeatModule'].ModuleScript.lua
    修复一个心跳的bug

--- code/['World']['Global']['Framework']['ClientHeartbeatModule'].ModuleScript.lua
+++ code/['World']['Global']['Framework']['ClientHeartbeatModule'].ModuleScript.lua
@@ -83,7 +83,7 @@ end
 --- 初始化事件和绑定Handler
 function InitEventsAndListeners()
     if localPlayer.C_Event == nil then
-        world:CreateObject('FolderObject', 'C_Event', localPlayer)
+        world:CreateObject('FolderObject', 'C_Event', localPlayer.C_Event)
     end
     world:CreateObject('CustomEvent', 'HeartbeatS2CEvent', localPlayer.C_Event)
     localPlayer.C_Event.HeartbeatS2CEvent:Connect(HeartbeatS2CEventHandler)
